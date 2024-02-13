6309bc8e34a6ad2c72203d1407af4dfcee8ee0f4
ginnnola.gomez <ginnolag@gmail.com>
2024-02-13 10:13:41 -0400

    Fake server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NuevoUsuarioComponent } from './components/dashboard/nuevo-usuario/nuevo-usuario.component';
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent}, 
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'nuevo', component: NuevoUsuarioComponent},
+    {path: '**', redirectTo: '/login'}
+];
